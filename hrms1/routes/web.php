07726fa6031b901e96c3d3a8b548fd4b23c7d0a9
k-hx <61220081+k-hx@users.noreply.github.com>
2021-08-15 00:01:56 +0800

    "create leave type" page

--- hrms1/routes/web.php
+++ hrms1/routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// create leave type -----------------------------------------------------------
+Route::get('createLeaveType', function() {
+   return view('createLeaveType');
+});
