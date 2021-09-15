1abb22416972760dac8090e56d205dbe9bf54883
k-hx <61220081+k-hx@users.noreply.github.com>
2021-09-15 22:20:25 +0800

    Seeder (payroll)

---
+++ hrms1/database/seeders/CategoryOfSalaryComponentSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoryOfSalaryComponentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('category_of_salary_components')->insert([
+          'name' => 'Allowances',
+      ]);
+
+      DB::table('category_of_salary_components')->insert([
+          'name' => 'Other Perquisites',
+      ]);
+
+      DB::table('category_of_salary_components')->insert([
+          'name' => 'Other Perquisites',
+      ]);
+
+      DB::table('category_of_salary_components')->insert([
+          'name' => 'Remuneration',
+      ]);
+
+      DB::table('category_of_salary_components')->insert([
+          'name' => 'Deductions',
+      ]);
+
+      DB::table('category_of_salary_components')->insert([
+          'name' => 'Benefits-in-Kind',
+      ]);
+    }
+}
